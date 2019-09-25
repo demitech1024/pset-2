c1b1d673e7276698f76e49c9bf6975d29fadbf8a
demitech1024 <demitech1024@gmail.com>
2019-09-25 13:58:01 -0400

    Finished exercise 2 and started exercise 3.

--- pset-2-complete/src/ProblemSet2.java
+++ pset-2-complete/src/ProblemSet2.java
@@ -15,6 +15,7 @@
  */
 
 import java.util.Scanner;
+import java.lang.Math;
 
 // import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 // import sun.swing.PrintColorUIResource;
@@ -31,20 +32,20 @@ public class ProblemSet2 {
          * name, grade, age, and hometown.
          */
         
-        System.out.print("\nEnter your first name: ");
-        String firstName = in.nextLine();
-        System.out.print("Enter your last name: ");
-        String lastName = in.nextLine();
-        System.out.print("Enter your grade: ");
-        String grade = in.nextLine();
-        System.out.print("Enter your age: ");
-        String age = in.nextLine();
-        System.out.print("Enter your hometown: ");
-        String homeTown = in.nextLine();
-        System.out.printf("\nNAME     : %s %s", firstName, lastName);
-        System.out.printf("\nGRADE    : %s", grade);
-        System.out.printf("\nAGE      : %s", age);
-        System.out.printf("\nHOMETOWN : %s\n\n", homeTown);
+        // System.out.print("\nEnter your first name: ");
+        // String firstName = in.nextLine();
+        // System.out.print("Enter your last name: ");
+        // String lastName = in.nextLine();
+        // System.out.print("Enter your grade: ");
+        // String grade = in.nextLine();
+        // System.out.print("Enter your age: ");
+        // String age = in.nextLine();
+        // System.out.print("Enter your hometown: ");
+        // String homeTown = in.nextLine();
+        // System.out.printf("\nNAME     : %s %s", firstName, lastName);
+        // System.out.printf("\nGRADE    : %s", grade);
+        // System.out.printf("\nAGE      : %s", age);
+        // System.out.printf("\nHOMETOWN : %s\n", homeTown);
 
         /*
          * Exercise 2.
@@ -52,8 +53,35 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
+        
+        final double dollarValue = 1.00;
+        final double quarterValue = 0.25;
+        final double dimeValue = 0.10;
+        final double nickelValue = 0.05;
+        final double pennyValue = 0.01;
 
+        System.out.print("\nEnter a dollar amount: ");
+        double dollarAmount = in.nextDouble();
 
+        int numDollar = (int) Math.floor(dollarAmount / dollarValue);
+        System.out.println("\nDOLLARS  : " + numDollar);
+        dollarAmount -= ((double) numDollar) * dollarValue;
+
+        int numQuarter = (int) Math.floor(dollarAmount / quarterValue);
+        System.out.println("QUARTERS : " + numQuarter);
+        dollarAmount -= ((double) numQuarter) * quarterValue;
+
+        int numDime = (int) Math.floor(dollarAmount / dimeValue);
+        System.out.println("DIMES    : " + numDime);
+        dollarAmount -= ((double) numDime) * dimeValue;
+
+        int numNickel = (int) Math.floor(dollarAmount / nickelValue);
+        System.out.println("NICKELS  : " + numNickel);
+        dollarAmount -= ((double) numNickel) * nickelValue;
+
+        int numPenny = (int) Math.floor(dollarAmount / pennyValue);
+        System.out.println("PENNIES  : " + numPenny);
+        
 
         /*
          * Exercise 3.
@@ -62,7 +90,11 @@ public class ProblemSet2 {
          * bills and coins needed to produce this amount.
          */
 
+        System.out.print("\nEnter a dollar amount: ");
 
+        System.out.println("BILLS : ");
+        System.out.println("COINS : ");
+        
 
         /*
          * Exercise 4.
